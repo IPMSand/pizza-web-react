9e76e3f36ff791f743bcdfa36a9b1008e3e5158d
IPMSand <131543563+IPMSand@users.noreply.github.com>
2025-09-06 12:18:00 +0530

    Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import Logo from '../assets/pi.avif';
 import '../styles/Navbar.css';
 
 function Navbar() {
   return (
     <div>
-        <img src={Logo} alt='Logo here' />
+       <div className='leftNav'>
+         <img src={Logo} alt='Logo here' />
+       </div>
+
+       <div className='rightNav'>
+          <Link to="/">Home </Link>
+          <Link to="/menu">Menu </Link>
+       </div>
+
+
     </div>
   )
 }
