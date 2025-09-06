2f05ea1a18ff9673897add103098264a02cbc113
IPMSand <131543563+IPMSand@users.noreply.github.com>
2025-09-06 12:09:20 +0530

    Routes ok

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+
+    <Router>
       <Navbar />
-      <header className="App-header">
-     <h1>Hello My React Set Up! meow</h1>
-      </header>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+      </Routes>
+   </Router>
+      
     </div>
   );
 }
 
 export default App;
+
+
 // < src={logo} className="App-logo" alt="logo" />
+/**
+ * <Navbar />
+      <header className="App-header">
+     <h1>Hello My React Set Up! meow</h1>
+      </header>
+ */
